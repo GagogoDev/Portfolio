544cc3dd3c66d2b2c77c6d35175948eb2901138a
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2023-01-30 08:03:28 -0300

    Cambio visual perfil

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -4,20 +4,16 @@ export const Perfil = () => {
             <div className="row d-flex justify-content-center align-items-center" id="section-perfil">
                 <div className="col-12">
                     <div className="row d-flex justify-content-center align-items-center text-center">
-                        <div className="col-12 col-lg-4 text-center text-lg-end">
-                            <img
-                                src="public/images/user-image-test.png"
-                                alt="Foto de perfil"
-                                id="foto-perfil"
-                            />
-                        </div>
-                        <div className="col-12 col-lg-8 text-center text-lg-start">
-                            <h1 className="d-inline text-primary">
-                                Gabriel Quiroz Ríos
-                            </h1>
-                            <br />
-                            <h5 className="d-inline">Programador Junior</h5>
-                        </div>
+                        <img
+                            src="public/images/user-image-test.png"
+                            alt="Foto de perfil"
+                            id="foto-perfil"
+                        />
+                        <h1 className="d-inline text-primary">
+                            Gabriel Quiroz Ríos
+                        </h1>
+                        <br />
+                        <h5 className="d-inline">Programador Junior</h5>
                     </div>
                     <div className="row mt-4 mt-lg-2 d-flex justify-content-center align-items-center text-center">
                         <div className="col-12 col-lg-6">
