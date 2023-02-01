f07405cbe692a2a1f068fbf46f91d8c380939caa
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2023-02-01 08:06:16 -0300

    Mejoras visuales perfil y navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export const Navbar = () => {
-
+    const [language, setLanguage] = useState(true)
     const { t, i18n } = useTranslation();
 
-    const onSwitchLanguage = ({ target }) => {
-        const checked = target.checked;
+    const onSwitchLanguage = () => {
+        setLanguage(!true)
 
-        if (checked) { // Ingles
+        if (language) { // Ingles
             i18n.changeLanguage('en');
         } else { // Español
             i18n.changeLanguage('es');
@@ -16,15 +17,15 @@ export const Navbar = () => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-darkv nav">
-            <div className="container-fluid">
+            <div className="container-fluid ps-3 ps-lg-5">
                 <a className="navbar-brand" href="#">
                     {'<GQ/>'}
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarPortfolio" aria-controls="navbarPortfolio" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end w-100">
+                <div className="collapse navbar-collapse" id="navbarPortfolio">
+                    <ul className="navbar-nav me-auto d-flex justify-content-end w-100">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">
                                 {t('Navbar.Perfil')}
@@ -51,16 +52,9 @@ export const Navbar = () => {
                             </button>
                         </li>
                         <li className="nav-item">
-                            <div className="nav-link">
-                                <div className="d-flex align-items-center">
-                                    <label>ES</label>
-                                    <div className="form-check form-switch d-flex align-items-center justify-content-center">
-                                        <input className="form-check-input" type="checkbox" id="switchLanguage" onChange={onSwitchLanguage}/>
-                                        <label className="form-check-label" htmlFor="switchLanguage"></label>
-                                    </div>
-                                    <label>EN</label>
-                                </div>
-                            </div>
+                            <a className="nav-link" onClick={onSwitchLanguage}>
+                                {t('Navbar.Idioma')}
+                            </a>
                         </li>
                     </ul>
                 </div>
--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,33 +1,18 @@
 export const Perfil = () => {
     return (
         <section>
-            <div className="row d-flex justify-content-center align-items-center" id="section-perfil">
-                <div class="bg-image"></div>
-
-                <div className="col-12 pt-5 pb-5 box-perfil">
-                    <div className="row d-flex justify-content-center align-items-center text-center">
-                        <img
-                            src="public/images/user-image-test.png"
-                            alt="Foto de perfil"
-                            id="foto-perfil"
-                        />
-                        <h1 className="d-inline text-primary">
-                            Gabriel Quiroz Ríos
-                        </h1>
-                        <br />
-                        <h5 className="d-inline">Programador Junior</h5>
-                    </div>
-                    <div className="row mt-4 mt-lg-2 d-flex justify-content-center align-items-center text-center">
-                        <div className="col-12 col-lg-6">
-                            <p>
-                                Reprehenderit dolore do sunt nisi qui aliqua ea
-                                aute eiusmod fugiat aute aute laborum. Cupidatat
-                                ut proident excepteur ad nisi amet in velit et.
-                                In non ut proident dolor nisi nostrud pariatur
-                                minim esse sit veniam.
-                            </p>
-                        </div>
-                    </div>
+            <div className="row" id="sectionPerfil">
+                <div className="col-12">
+                    <p className="title d-block text-primary">
+                        Gabriel Quiroz Ríos
+                    </p>
+                    <p className="subtitle d-block">Programador Junior</p>
+                    <p className="description d-block">
+                        Reprehenderit dolore do sunt nisi qui aliqua ea aute
+                        eiusmod fugiat aute aute laborum. Cupidatat ut proident
+                        excepteur ad nisi amet in velit et. In non ut proident
+                        dolor nisi nostrud pariatur minim esse sit veniam.
+                    </p>
                 </div>
             </div>
         </section>
