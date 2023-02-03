4f6ca4f12dd62b07d7d043f7f538678ed008e9a7
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2023-02-03 19:27:14 -0300

    Se ajusta diseño de navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,33 @@
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const Navbar = () => {
-    const [language, setLanguage] = useState(true)
+    const [language, setLanguage] = useState(true);
     const { t, i18n } = useTranslation();
 
     const onSwitchLanguage = () => {
-        setLanguage(!true)
+        setLanguage(!true);
 
-        if (language) { // Ingles
+        if (language) {
+            // Ingles
             i18n.changeLanguage('en');
-        } else { // Español
+        } else {
+            // Español
             i18n.changeLanguage('es');
         }
-    }
+    };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-darkv nav">
-            <div className="container-fluid ps-3 ps-lg-5">
+        <nav className="navbar navbar-expand-lg navbar-dark nav">
+            <div className="container-fluid">
                 <a className="navbar-brand" href="#">
                     {'<GQ/>'}
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarPortfolio" aria-controls="navbarPortfolio" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarPortfolio">
-                    <ul className="navbar-nav me-auto d-flex justify-content-end w-100">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" >
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end w-100">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">
                                 {t('Navbar.Perfil')}
