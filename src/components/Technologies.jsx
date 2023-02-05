35d094c0561cfb7874af097357713e27d0d893ee
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2023-02-05 18:03:10 -0300

    Se ajustan estilos

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,9 +3,9 @@ import { Carousel } from './Carousel';
 export const Technologies = () => {
     return (
         <section>
-            <div className="row" id="section-technologies">
+            <div className="row d-flex justify-content-center" id="section-technologies">
                 <h1 className="h1-technologies text-center text-primary">Tecnologías</h1>
-                <div className="col-12 d-flex justify-content-center mt-5">
+                <div className="col-12 col-lg-10 d-flex justify-content-center mt-5">
                     <Carousel />
                 </div>
             </div>
