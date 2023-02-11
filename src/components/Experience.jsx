b4e239dc113d136d14036bd86ef4b4457245abf6
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2023-02-11 15:45:44 -0300

    Maquetación de linea de tiempo experiencia

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,49 @@
 export const Experience = () => {
     return (
         <section>
-            <div className="row d-flex justify-content-center" id="section-experience">
-                <h1 className="h1-experience text-center text-primary">Experiencia</h1>
+            <div className="row d-flex justify-content-center" id="section-experience" >
+                <h1 className="h1-experience text-center text-primary">
+                    Experiencia
+                </h1>
                 <div className="col-12 col-lg-10 d-flex justify-content-center mt-5">
-                    
+                    <ul className="timeline">
+                        <li>
+                            <div className="direction-r">
+                                <div className="flag-wrapper">
+                                    <span className="hexa"></span>
+                                    <span className="flag">Impact</span>
+                                    <span className="time-wrapper">
+                                        <span className="time">
+                                            Feb 2015 - Mar 2022
+                                        </span>
+                                    </span>
+                                </div>
+                                <div className="desc">
+                                    Lorem ipsum Nisi labore aute do aute culpa
+                                    magna nulla voluptate exercitation deserunt
+                                    proident.
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="direction-l">
+                                <div className="flag-wrapper">
+                                    <span className="hexa"></span>
+                                    <span className="flag">
+                                        Lorem ipsum Anim.
+                                    </span>
+                                    <span className="time-wrapper">
+                                        <span className="time">Dec 2014</span>
+                                    </span>
+                                </div>
+                                <div className="desc">
+                                    Lorem ipsum In ut sit in dolor nisi ex magna
+                                    eu anim anim tempor dolore aliquip enim
+                                    cupidatat laborum dolore.
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </section>
