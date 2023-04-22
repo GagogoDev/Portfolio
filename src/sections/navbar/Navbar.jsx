4e1f14543e83a078a961f668c468c5c2c345d864
Gabriel Quiroz <gabriel.quiroz@impact.cl>
2023-04-22 14:53:41 -0400

    Se añade efecto en links navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -18,27 +18,27 @@ export const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarPortfolio" >
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end w-100">
                         <li className="nav-item">
-                            <Link className="nav-link" to="section-profile" smooth={true} offset={0} duration={100} >
+                            <Link className="nav-link text-effect" to="section-profile" smooth={true} offset={0} duration={100} >
                                 {t('Navbar.Profile')}
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="section-technologies" smooth={true} offset={-200} duration={100} >
+                            <Link className="nav-link text-effect" to="section-technologies" smooth={true} offset={-200} duration={100} >
                                 {t('Navbar.Technologies')}
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="section-experience" smooth={true} offset={-200} duration={100} >
+                            <Link className="nav-link text-effect" to="section-experience" smooth={true} offset={-200} duration={100} >
                                 {t('Navbar.Experience')}
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="section-education" smooth={true} offset={-200} duration={100} >
+                            <Link className="nav-link text-effect" to="section-education" smooth={true} offset={-200} duration={100} >
                                 {t('Navbar.Education')}
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="section-contact" smooth={true} offset={-100} duration={100} >
+                            <Link className="nav-link text-effect" to="section-contact" smooth={true} offset={-100} duration={100} >
                                 {t('Navbar.Contact')}
                             </Link>
                         </li>
